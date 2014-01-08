ace20398e047f526476503508fa6994ea0352480
James Smith <james@floppy.org.uk>
2014-01-08 18:35:08 +0000

    Add redistogo for data storage

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby "2.1.0"
 
 gem "rake"
 gem "sinatra"
+gem "redis"
 
 group :development do
   gem "travis"
--- votebot.rb
+++ votebot.rb
@@ -1,4 +1,12 @@
 require 'sinatra/base'
+require 'redis'
+
+if ENV['RACK_ENV'] != "production" 
+  ENV["REDISTOGO_URL"] = 'redis://localhost'
+end
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 class Votebot < Sinatra::Base
   
