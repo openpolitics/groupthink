7d9e9a0d42a4b9ac36458d09f58ba150cdea6b7c
James Smith <james@floppy.org.uk>
2014-01-08 18:33:22 +0000

    Accept github webhook POSTs

--- spec/requests/index_spec.rb
+++ spec/requests/index_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'json'
 
 describe "index page" do
   
@@ -8,4 +9,27 @@ describe "index page" do
     last_response.body.should include "http://openpolitics.org.uk"
   end
   
+  it "/ should reject unknown posts" do
+    post '/'
+    last_response.should be_bad_request
+  end
+  
+  it "/ should accept Github issue comment posts" do
+    header 'X-Github-Event', "issue_comment"
+    post '/'
+    last_response.should be_ok
+  end
+
+  it "/ should accept Github pull request posts" do
+    header 'X-Github-Event', "pull_request"
+    post '/'
+    last_response.should be_ok
+  end
+
+  it "/ should not accept other Github posts" do
+    header 'X-Github-Event', "something_else"
+    post '/'
+    last_response.should be_bad_request
+  end
+
 end
--- votebot.rb
+++ votebot.rb
@@ -6,4 +6,15 @@ class Votebot < Sinatra::Base
     "Nothing to see here; visit <a href='http://openpolitics.org.uk'>openpolitics.org.uk</a> instead!"
   end
   
+  post '/' do
+    case env['HTTP_X_GITHUB_EVENT']
+    when "issue_comment"
+      200
+    when "pull_request"
+      200
+    else
+      400
+    end
+  end
+  
 end
