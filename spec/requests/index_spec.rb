de196ed358941a30454f4b3b24251bdc62380973
James Smith <james@floppy.org.uk>
2014-01-09 06:59:46 +0000

    split webhook tests out

--- spec/requests/index_spec.rb
+++ spec/requests/index_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'json'
 
 describe "index page" do
   
@@ -9,35 +8,4 @@ describe "index page" do
     last_response.body.should include "http://openpolitics.org.uk"
   end
   
-  it "/ should reject unknown posts" do
-    post '/'
-    last_response.should be_bad_request
-  end
-  
-  it "/ should parse github issue comments correctly" do
-    # Should result in PR 32 being updated
-    Votebot.any_instance.should_receive(:update_pr).with(32).once
-    # Set POST
-    header 'X-Github-Event', "issue_comment"
-    post '/', payload: load_fixture('requests/issue_comment')
-    # Check response
-    last_response.should be_ok
-  end
-
-  it "/ should parse github pull requests correctly" do
-    # Should result in PR 43 being updated
-    Votebot.any_instance.should_receive(:update_pr).with(43).once
-    # Set POST
-    header 'X-Github-Event', "pull_request"
-    post '/', payload: load_fixture('requests/pull_request')
-    # Check response
-    last_response.should be_ok
-  end
-
-  it "/ should not accept other Github posts" do
-    header 'X-Github-Event', "something_else"
-    post '/'
-    last_response.should be_bad_request
-  end
-
 end
