cb877d8a832271f603453b0368adb6159bce1fd3
James Smith <james@floppy.org.uk>
2016-07-27 22:30:44 +0100

    Add user editing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_filter :get_user, except: [:index]
+  before_action :authorise, only: [:edit, :update]
   
   def index
     @users = User.all.order(:login)
@@ -7,11 +10,34 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_login(params[:id])
     @proposals = Proposal.all.sort_by{|x| x.number.to_i}.reverse
     @proposed, @proposals = @proposals.partition{|x| x.proposer == @user}
     @voted, @not_voted = @proposals.partition{|pr| @user.participating.where("last_vote IS NOT NULL").include? pr}
     @not_voted.reject!{|x| x.closed? }
+  end
+
+  def edit
+  end
+  
+  def update
+    @user.update_attributes!(user_params)
+    redirect_to edit_user_path(@user)
+  end
+
+private
+
+  def get_user
+    @user = User.find_by_login(params[:id])
+  end
+
+  def authorise
+    if @user != current_user
+      redirect_to @user
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :notify_new)
   end
 
 end
