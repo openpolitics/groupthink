eec5e2d8e4db5596972e1e69f21f0d0e53f31d3f
James Smith <james@floppy.org.uk>
2016-07-31 21:25:48 +0100

    refactor index test into controller specs

--- spec/controllers/proposals_controller_spec.rb
+++ spec/controllers/proposals_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProposalsController, type: :controller do
+  render_views
+  
+  it "should show index page" do
+    get :index
+    expect(response).to be_ok
+    expect(response.body).to include "http://openpolitics.org.uk"
+  end
 
 end
--- spec/integration/index_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe "index page" do
-  
-  it "should allow accessing the home page" do
-    get '/'
-    expect(response).to be_ok
-    expect(response.body).to include "http://openpolitics.org.uk"
-  end
-  
-end
