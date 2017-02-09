a139bd2cf8a38fca58642891a19ad68086a4d785
James Smith <james@floppy.org.uk>
2017-02-09 00:09:00 +0000

    missing email setting

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@openpolitics.org.uk'
+  config.mailer_sender = "no-reply@#{ENV['EMAIL_DOMAIN']}"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
