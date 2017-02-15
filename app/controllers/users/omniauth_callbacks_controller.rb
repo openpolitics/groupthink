36fc05f1bff0f95a209faa79d27b461b77108bec
James Smith <james@floppy.org.uk>
2017-02-15 09:50:57 +0000

    store github token in session after login
    without this I’m not sure how it ever worked. Resolves #51

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = User.from_omniauth(request.env["omniauth.auth"])
 
       if @user.persisted?
+        session[:github_token] = request.env["omniauth.auth"]['credentials']['token']
         sign_in @user, :event => :authentication #this will throw if @user is not activated
         set_flash_message(:notice, :success, :kind => "GitHub") if is_navigational_format?        
         if session[:original_path]
