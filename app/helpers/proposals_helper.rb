8763a586896b8187e3aaa1060227d501311ce2a7
James Smith <james@floppy.org.uk>
2017-02-26 22:02:28 +0000

    autolink proposal numbers and usernames

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -3,20 +3,37 @@ module ProposalsHelper
   def render_github_markdown(markdown)
     markdown = replace_emoji(markdown)
     markdown = GitHub::Markup.render('comment.markdown', markdown).html_safe
+    markdown = link_usernames(markdown)
+    markdown = link_proposals(markdown)
     markdown = auto_link(markdown)
   end
 
-  def replace_emoji(markdown)
+  def replace_emoji(str)
     {
       ":thumbsup:" => "👍",
       ":thumbsdown:" => "👎",
       ":+1:" => "👍",
       ":-1:" => "👎",
       ":hand:" => "✋",
+      ":smiley:" => "😃",
     }.each_pair do |before, after|
-      markdown = markdown.gsub(before, after)
+      str = str.gsub(before, after)
     end
-    markdown
+    str
+  end
+
+  def link_usernames(str)
+    str.scan(/(\s|^|\>)@(\w+)/).each do |match|
+      str = str.gsub "@#{match[1]}", "<a href='/users/#{match[1]}'>@#{match[1]}</a>"
+    end
+    str.html_safe
+  end
+  
+  def link_proposals(str)
+    str.scan(/\s?#(\d+)([^0-9;]|$)/).each do |match|
+      str = str.gsub "##{match[0]}", "<a href='/proposals/#{match[0]}'>##{match[0]}</a>"
+    end
+    str.html_safe
   end
 
 end
--- spec/helpers/proposals_helper_spec.rb
+++ spec/helpers/proposals_helper_spec.rb
@@ -11,4 +11,83 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe ProposalsHelper, type: :helper do
+  
+  context "replacing emoji" do
+    {
+      ":thumbsup:" => "👍",
+      ":thumbsdown:" => "👎",
+      ":+1:" => "👍",
+      ":-1:" => "👎",
+      ":hand:" => "✋",
+      ":smiley:" => "😃",
+    }.each_pair do |input, output|
+      it "#{input} with #{output}" do
+        expect(helper.replace_emoji(input)).to eql output
+      end
+    end
+  end
+  
+  context "linking usernames" do
+    
+    it "at the start of a string" do
+      input = "@hello there"
+      output = "<a href='/users/hello'>@hello</a> there"
+      expect(helper.link_usernames(input)).to eql output
+    end
+        
+    it "in the middle of a string" do
+      input = "well @hello there"
+      output = "well <a href='/users/hello'>@hello</a> there"
+      expect(helper.link_usernames(input)).to eql output
+    end
+
+    it "at the end of a string" do
+      input = "well @hello"
+      output = "well <a href='/users/hello'>@hello</a>"
+      expect(helper.link_usernames(input)).to eql output
+    end
+
+    it "at the start of a paragraph tag" do
+      input = "<p>@hello there</p>"
+      output = "<p><a href='/users/hello'>@hello</a> there</p>"
+      expect(helper.link_usernames(input)).to eql output
+    end
+
+    it "not in an email address" do
+      input = "bob@example.com"
+      output = "bob@example.com"
+      expect(helper.link_usernames(input)).to eql output
+    end
+
+  end
+
+  context "linking proposals" do
+    
+    it "at the start of a string" do
+      input = "#434 is relevant"
+      output = "<a href='/proposals/434'>#434</a> is relevant"
+      expect(helper.link_proposals(input)).to eql output
+    end
+        
+    it "in the middle of a string" do
+      input = "I think #434 is relevant"
+      output = "I think <a href='/proposals/434'>#434</a> is relevant"
+      expect(helper.link_proposals(input)).to eql output
+    end
+
+    it "at the end of a string" do
+      input = "see #434"
+      output = "see <a href='/proposals/434'>#434</a>"
+      expect(helper.link_proposals(input)).to eql output
+    end
+
+    it "at the start of a paragraph tag" do
+      input = "<p>#434 is relevant</p>"
+      output = "<p><a href='/proposals/434'>#434</a> is relevant</p>"
+      expect(helper.link_proposals(input)).to eql output
+    end
+
+
+  end
+
 end
