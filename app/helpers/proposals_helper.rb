97394767e99d9506834d41e4de18feb2c50407d6
James Smith <james@floppy.org.uk>
2017-02-26 19:57:16 +0000

    replace missing emoji in markdown renderer

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -1,7 +1,22 @@
 module ProposalsHelper
 
   def render_github_markdown(markdown)
-    auto_link(GitHub::Markup.render('comment.markdown', markdown).html_safe)
+    markdown = replace_emoji(markdown)
+    markdown = GitHub::Markup.render('comment.markdown', markdown).html_safe
+    markdown = auto_link(markdown)
+  end
+
+  def replace_emoji(markdown)
+    {
+      ":thumbsup:" => "👍",
+      ":thumbsdown:" => "👎",
+      ":+1:" => "👍",
+      ":-1:" => "👎",
+      ":hand:" => "✋",
+    }.each_pair do |before, after|
+      markdown = markdown.gsub(before, after)
+    end
+    markdown
   end
 
 end
