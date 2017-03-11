a646021e02bc5761cc96995616e88b91cc8e00fe
James Smith <james@floppy.org.uk>
2017-03-11 12:12:02 +0000

    Form for new action
    Auto-generate filename from title in form

--- app/controllers/edit_controller.rb
+++ app/controllers/edit_controller.rb
@@ -7,12 +7,20 @@ class EditController < ApplicationController
   def index
   end
   
+  def new
+    @content = ""
+  end
+  
   def edit    
     @content = get_files(original_repo_path, @filename)[@filename]
     @lineendings = detect_line_endings(@content)
   end
 
   def message
+    # Prepend title if we have been given one, for a new filename
+    if @title.present?
+      @content = "---\ntitle: #{@title}\n---\n#{@content}"
+    end
     # Prepare a fork if we don't have permission to push
     unless github.repository(original_repo_path).permissions.push
       github.fork original_repo_path
@@ -20,6 +28,7 @@ class EditController < ApplicationController
   end
   
   def commit
+    raise "parp"
     # Fix line endings
     @content = convert_line_endings(@content, @lineendings)
     # Do we need to work in a fork?
@@ -56,8 +65,12 @@ class EditController < ApplicationController
     @owner = params[:owner]
     @repo = params[:repo]
     @branch = params[:branch]
+    @title = params[:title]
     @filename = params[:filename] || params[:path]
     @format = params[:format] || @filename ? @filename.split('.').last : "md"
+    if @title.present? && @filename.nil?
+      @filename = @title.parameterize + ".#{@format}"
+    end
     @content = params[:content]
     @summary = params[:summary]
     @description = params[:description]
