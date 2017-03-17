0e9199a70072a32363d0670c5915310cbc5d818f
James Smith <james@floppy.org.uk>
2017-03-17 16:46:24 +0000

    stub one github test

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe IdeasController, type: :controller, vcr: true do
+RSpec.describe IdeasController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
+      allow_any_instance_of(Octokit::Client).to receive(:issues).and_return([])
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #show" do
-    it "returns http success" do
+    it "returns http success", :vcr do
       get :show, params: {id: 430}
       expect(response).to have_http_status(:success)
     end
