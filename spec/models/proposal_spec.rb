27890f9f6c93ff8bec1a6fb1a2deeefc1939f532
James Smith <james@floppy.org.uk>
2017-03-17 16:32:42 +0000

    split proposal specs into proposal and vote counter

--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -6,43 +6,7 @@ RSpec.describe Proposal, :vcr do
     pr = Proposal.create(number: 43)
     expect(pr.proposer.login).to eq 'Floppy'
   end
-
-  it "should only count latest vote per person" do
-    pr = Proposal.create(number: 100)
-    expect(pr.yes.count).to eq 2
-    expect(pr.yes.map{|x| x.user.login}.sort).to eq ["Floppy", "philipjohn"]
-  end
-
-  it "should handle both thumbsup and +1 emoticons as yes votes" do
-    pr = Proposal.create(number: 356)
-    expect(pr.yes.map{|x| x.user.login}.sort).to eq ["Floppy", "philipjohn"]
-  end
-
-  it "should handle emoji yes votes" do
-    pr = Proposal.create(number: 433)
-    expect(pr.yes.map{|x| x.user.login}.sort).to eq ["Floppy"]
-  end
-
-  it "should ignore votes from proposer" do
-    pr = Proposal.create(number: 74)
-    expect(pr.yes.count).to eq 0
-  end
-
-  it "should ignore votes before last commit" do
-    pr = Proposal.create(number: 135)
-    expect(pr.yes.count).to eq 1
-  end
-
-  it "should store merged pull requests as accepted" do
-    pr = Proposal.create(number: 43)
-    expect(pr.state).to eq 'accepted'
-  end
-
-  it "should store closed and unmerged pull requests as rejected" do
-    pr = Proposal.create(number: 9)
-    expect(pr.state).to eq 'rejected'
-  end
-
+  
   context "notification of new proposals" do
   
     before :all do
