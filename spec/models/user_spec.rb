9aadca087d055afccbe73f8bc9702c041ec0b0db
James Smith <james@floppy.org.uk>
2017-03-18 20:52:58 +0000

    improve user github data test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User do
 
-  it "should load user data from github", :vcr do
-    u = User.create(login: 'Floppy')
-    expect(u.avatar_url).to be_present
-    expect(u.email).to eq 'james@floppy.org.uk'
+  it "should fill in extra user data from github on creation" do
+    login = "test"
+    expect(Octokit).to receive(:user).with(login).and_return(
+      OpenStruct.new({
+        avatar_url: "https://example.com/avatar.png",
+        email: "test@example.com"
+      })
+    )
+    expect(Octokit).to receive(:contributors).and_return([
+      OpenStruct.new({
+        login: login
+      })
+    ])
+    u = create :user_with_github_data, login: login
+    expect(u.avatar_url).to eq "https://example.com/avatar.png"
+    expect(u.email).to eq "test@example.com"
     expect(u.contributor).to eq true
   end
 
