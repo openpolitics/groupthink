3ae9826aeca06aa21d283e887148eefda16419b1
James Smith <james@floppy.org.uk>
2017-03-18 21:12:45 +0000

    remove VCR from ideas controller spec

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -11,7 +11,17 @@ RSpec.describe IdeasController, type: :controller do
   end
 
   describe "GET #show" do
-    it "returns http success", :vcr do
+    it "returns http success" do
+      login = "test"
+      allow_any_instance_of(Octokit::Client).to receive(:issue).with(ENV["GITHUB_REPO"], 430).and_return(
+        OpenStruct.new({
+          user: OpenStruct.new({
+            login: login
+          })
+        })
+      )
+      expect(User).to receive(:find_or_create_by).with(login: login).and_return(create :user, login: login)
+      allow_any_instance_of(Octokit::Client).to receive(:issue_comments).with(ENV["GITHUB_REPO"], 430).and_return([])
       get :show, params: {id: 430}
       expect(response).to have_http_status(:success)
     end
