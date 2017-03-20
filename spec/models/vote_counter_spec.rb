5785ed1dea6a2aca8dc9f7f360178f547257b481
James Smith <james@floppy.org.uk>
2017-03-20 17:10:54 +0000

    fix test flicker

--- spec/models/vote_counter_spec.rb
+++ spec/models/vote_counter_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe VoteCounter do
 
-  before :all do
+  before :each do
     @voter1 = create :user, contributor: true
     @voter2 = create :user, contributor: true
   end
