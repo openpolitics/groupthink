9f6e9d556da6cdeaeeff142454412eeac222b7ed
James Smith <james@floppy.org.uk>
2017-04-25 21:53:20 +0100

    use inline for tests
    because otherwise our expectations get confused

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  
+  # Process delayed jobs inline
+  config.active_job.queue_adapter   = :inline
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
