afeeaa9f0e882990931d1a2f4e08e5d238b52be5
James Smith <james@floppy.org.uk>
2019-04-04 15:45:10 +0100

    rubocop: refactor user model spec to shorten example

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,27 @@
 require "rails_helper"
 
 RSpec.describe User do
-  it "fills in extra user data from github on creation" do
-    login = "test"
-    expect(Octokit).to receive(:user).with(login).and_return(
-      OpenStruct.new(
-        avatar_url: "https://example.com/avatar.png",
-        email: "test@example.com"
-      )
+  let!(:login) { "test" }
+  let!(:mock_user) {
+    OpenStruct.new(
+      avatar_url: "https://example.com/avatar.png",
+      email: "test@example.com"
     )
-    expect(Octokit).to receive(:contributors).and_return([
+  }
+  let!(:mock_contributors) {
+    [
       OpenStruct.new(
         login: login
       )
-    ])
+    ]
+  }
+
+  before do
+    allow(Octokit).to receive(:user).with(login).and_return(mock_user)
+    allow(Octokit).to receive(:contributors).and_return(mock_contributors)
+  end
+
+  it "fills in extra user data from github on creation" do
     u = create :user_with_github_data, login: login
     expect(u.avatar_url).to eq "https://example.com/avatar.png"
     expect(u.email).to eq "test@example.com"
