c95e9d5e98d283aa5452b280450bc5fc7343489a
James Smith <james@floppy.org.uk>
2019-04-04 11:30:49 +0100

    move webhook tests into the right controller spec

--- spec/controllers/proposals_controller_spec.rb
+++ spec/controllers/proposals_controller_spec.rb
@@ -44,4 +44,53 @@ RSpec.describe ProposalsController, type: :controller do
       expect(response.redirect_url).to eq "http://test.host/proposals/#{@proposal.number}"
     end
   end
+
+  describe "webhook" do
+    it "/ should reject unknown posts" do
+      post "/webhook"
+      expect(response).to be_bad_request
+    end
+
+    it "/ should parse github issue comments correctly" do
+      # Set POST
+      post "/webhook",
+        params: { payload: load_fixture("requests/issue_comment") },
+        headers: { "X-Github-Event" => "issue_comment" }
+      # Check response
+      expect(response).to be_ok
+      # Should result in PR 32 being updated
+      expect(UpdateProposalJob).to have_been_enqueued.with(32)
+    end
+
+    it "/ should parse github pull requests correctly" do
+      Timecop.freeze
+      # Set POST
+      post "/webhook",
+        params: { payload: load_fixture("requests/pull_request") },
+        headers: { "X-Github-Event" => "pull_request" }
+      # Check response
+      expect(response).to be_ok
+      # Should result in PR 43 being updated
+      expect(CreateProposalJob).to have_been_enqueued.with(43)
+      expect(CreateProposalJob).to have_been_enqueued.at(5.seconds.from_now)
+      Timecop.return
+    end
+
+    it "/ should handle pull request closes correctly" do
+      # Set POST
+      post "/webhook",
+        params: { payload: load_fixture("requests/close_pull_request") },
+        headers: { "X-Github-Event" => "pull_request" }
+      # Check response
+      expect(response).to be_ok
+      # Should result in PR 43 being closed
+      expect(CloseProposalJob).to have_been_enqueued.with(43)
+    end
+
+    it "/ should not accept other Github posts" do
+      post "/webhook",
+        headers: { "X-Github-Event" => "something_else" }
+      expect(response).to be_bad_request
+    end
+  end
 end
