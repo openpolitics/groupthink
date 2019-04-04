e92ec1fefc65c7f77af5cb5ce8b0c8080483499c
James Smith <james@floppy.org.uk>
2019-04-04 15:27:06 +0100

    update webhook tests after move into controller

--- spec/controllers/proposals_controller_spec.rb
+++ spec/controllers/proposals_controller_spec.rb
@@ -47,15 +47,15 @@ RSpec.describe ProposalsController, type: :controller do
 
   describe "webhook" do
     it "/ should reject unknown posts" do
-      post "/webhook"
+      post :webhook
       expect(response).to be_bad_request
     end
 
     it "/ should parse github issue comments correctly" do
       # Set POST
-      post "/webhook",
-        params: { payload: load_fixture("requests/issue_comment") },
-        headers: { "X-Github-Event" => "issue_comment" }
+      @request.headers["X-Github-Event"] = "issue_comment"
+      post :webhook,
+        params: { payload: load_fixture("requests/issue_comment") }
       # Check response
       expect(response).to be_ok
       # Should result in PR 32 being updated
@@ -65,9 +65,9 @@ RSpec.describe ProposalsController, type: :controller do
     it "/ should parse github pull requests correctly" do
       Timecop.freeze
       # Set POST
-      post "/webhook",
-        params: { payload: load_fixture("requests/pull_request") },
-        headers: { "X-Github-Event" => "pull_request" }
+      @request.headers["X-Github-Event"] = "pull_request"
+      post :webhook,
+        params: { payload: load_fixture("requests/pull_request") }
       # Check response
       expect(response).to be_ok
       # Should result in PR 43 being updated
@@ -78,9 +78,9 @@ RSpec.describe ProposalsController, type: :controller do
 
     it "/ should handle pull request closes correctly" do
       # Set POST
-      post "/webhook",
-        params: { payload: load_fixture("requests/close_pull_request") },
-        headers: { "X-Github-Event" => "pull_request" }
+      @request.headers["X-Github-Event"] = "pull_request"
+      post :webhook,
+        params: { payload: load_fixture("requests/close_pull_request") }
       # Check response
       expect(response).to be_ok
       # Should result in PR 43 being closed
@@ -88,8 +88,8 @@ RSpec.describe ProposalsController, type: :controller do
     end
 
     it "/ should not accept other Github posts" do
-      post "/webhook",
-        headers: { "X-Github-Event" => "something_else" }
+      @request.headers["X-Github-Event"] = "something_else"
+      post :webhook
       expect(response).to be_bad_request
     end
   end
