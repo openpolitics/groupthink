44e17a9aa27a42a0ffdbc8f4508f8c0c381113f0
James Smith <james@floppy.org.uk>
2019-05-01 22:22:24 +0100

    document string extensions

--- lib/string_extensions.rb
+++ lib/string_extensions.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
+
+#
+# Additions to built-in String class to make it easy to check for cast votes.
+#
 class String
+  #
+  # Does the string contain a "yes" vote?
+  # Checks for ✅ and 👍 symbols, and old-style github emoji code equivalents
+  #
+  # @return [boolean] True if the string contains a yes vote
+  #
   def contains_yes?
     [
       ":white_check_mark:",
@@ -11,6 +21,12 @@ class String
     ].any? { |x| self.include?(x) }
   end
 
+  #
+  # Does the string contain a "no" vote?
+  # Checks for ❎ and ✋ symbols, and old-style github emoji code equivalents
+  #
+  # @return [boolean] True if the string contains a no vote
+  #
   def contains_no?
     [
       ":negative_squared_cross_mark:",
@@ -20,6 +36,12 @@ class String
     ].any? { |x| self.include?(x) }
   end
 
+  #
+  # Does the string contain an abstention?
+  # Checks for 🤐, and old-style github emoji code equivalent
+  #
+  # @return [boolean] True if the string contains an abstention
+  #
   def contains_abstention?
     [
       ":zipper_mouth_face:",
@@ -27,6 +49,12 @@ class String
     ].any? { |x| self.include?(x) }
   end
 
+  #
+  # Does the string contain an "block" vote?
+  # Checks for 🚫 and 👎, and old-style github emoji code equivalent
+  #
+  # @return [boolean] True if the string contains an block vote
+  #
   def contains_block?
     [
       ":no_entry_sign:",
