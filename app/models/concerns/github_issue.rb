bd0fe1f8298428ec1def0cc908da6f1cc1fbacf8
James Smith <james@floppy.org.uk>
2019-05-08 22:38:01 +0100

    add doc for github issue concern

--- app/models/concerns/github_issue.rb
+++ app/models/concerns/github_issue.rb
@@ -6,12 +6,22 @@
 module GithubIssue
   extend ActiveSupport::Concern
 
+  #
+  # Fetch comments from GitHub
+  #
+  # @return [Array] A list of comments
+  #
   def github_comments
     Octokit.issue_comments(ENV.fetch("GITHUB_REPO"), number)
   end
 
   private
 
+    #
+    # Add a comment to the GitHub issue
+    #
+    # @param [String] body The text of the comment to add
+    #
     def github_add_comment(body)
       Octokit.add_comment(ENV.fetch("GITHUB_REPO"), number, body)
     end
