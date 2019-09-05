e2340967eca251f3bbdcc493c2f555d2d46e17e5
James Smith <james@floppy.org.uk>
2019-09-05 22:56:52 +0100

    fix sqlite3 boolean flag deprecation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,4 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-
-  config.active_record.sqlite3.represent_boolean_as_integer = true
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,6 +45,4 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-
-  config.active_record.sqlite3.represent_boolean_as_integer = true
 end
