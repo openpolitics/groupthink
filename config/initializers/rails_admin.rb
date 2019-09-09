6d7755e8f9219ea01d6dac3d5e43d843d9c2af29
James Smith <james@floppy.org.uk>
2019-09-09 21:42:46 +0100

    configure and mount rails_admin

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,26 @@
+RailsAdmin.config do |config|
+
+  # == Devise ==
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
+
+  # == CancanCan ==
+  config.authorize_with :cancancan
+
+  # == PaperTrail ==
+  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+
+  config.actions do
+    dashboard
+    index
+    show
+    edit
+    show_in_app
+
+    ## With an audit adapter, you can add:
+    # history_index
+    # history_show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :users
 
   # Login
