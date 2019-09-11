be0255cf115725d278cf0b9de81c50f1170ad968
James Smith <james@floppy.org.uk>
2019-09-11 12:24:33 +0100

    add proper 404 handling - resolves #207

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@
 # @!visibility private
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_error
+
+  protected
+
+  def not_found_error
+    render file: 'public/404', status: :not_found
+  end
+
 end
