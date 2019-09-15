fab17edf3bde071c5dcd0bc562b69d0fc308cefc
James Smith <james@floppy.org.uk>
2019-09-15 11:15:58 +0100

    properly coerce concurrency settings into integers for puma

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,8 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+max_threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS") { 5 })
+min_threads_count = Integer(ENV.fetch("RAILS_MIN_THREADS") { max_threads_count })
 threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
@@ -25,7 +25,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
