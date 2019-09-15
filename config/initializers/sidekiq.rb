bce74e26ba669b3315dfdeec436eb63a80aa86fa
James Smith <james@floppy.org.uk>
2019-09-15 11:40:30 +0100

    configure server concurrency in YAML file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,3 @@
 Sidekiq.configure_client do |config|
   config.redis = { size: 5, url: ENV["REDIS_URL"] }
 end
-
-Sidekiq.configure_server do |config|
-  config.redis = { concurrency: 5, url: ENV["REDIS_URL"] }
-end
-
