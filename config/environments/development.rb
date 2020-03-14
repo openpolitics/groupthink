274ced28e4ec907b1201caa33e437b9df3ed49b7
James Smith <james@floppy.org.uk>
2020-03-14 00:28:45 +0000

    Swap from sidekiq to sucker_punch

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  config.active_job.queue_adapter = :sucker_punch
+
 end
