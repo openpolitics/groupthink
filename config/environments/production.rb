009f7d9bf36f79844c9c84cff8973a2b69aa253f
James Smith <james@floppy.org.uk>
2021-05-06 17:30:08 +0100

    fix lint issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,4 @@ Rails.application.configure do
   # these configuration options.
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
-  # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -29,7 +29,7 @@ unless Rails.env.test?
         active: true,
       }
     )
-  rescue Octokit::UnprocessableEntity => e
+  rescue Octokit::UnprocessableEntity
     # The hook already exists, no problem
     nil
   end
@@ -49,8 +49,9 @@ unless Rails.env.test?
 description: "Proposals to be voted on in Groupthink")
     create_label_if_missing(label: "groupthink::idea", colour: "fbca04",
 description: "Ideas for future proposals")
-  rescue Octokit::UnprocessableEntity => e
-    # We couldn't set the repo settings, but we don't want this to kill startup, so carry on regardless
+  rescue Octokit::UnprocessableEntity
+    # We couldn't set the repo settings, but we don't want this to kill
+    # startup, so carry on regardless
     nil
   end
 end
